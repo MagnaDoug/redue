8967e432fa678cd66703605d48ac91c65a22dd7b
Doug <doug87@live.com>
2014-07-26 15:25:19 -0600

    Add a full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
+  get "static_pages/contact"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
+  # You can have the root of ygour site routed with "root"
   # root 'welcome#index'
 
   # Example of regular route:
